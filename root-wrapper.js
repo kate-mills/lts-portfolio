3bb27c1e9808dee13ccd517840983eb0b837f7b5
Kate Mills <helloworldloveksm@icloud.com>
2023-07-09 13:18:34 -0700

    import 400 & 600 weight font only

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
 import '@fontsource/shadows-into-light-two'
-/*import '@fontsource/inter/400.css'
-import '@fontsource/inter/600.css'*/
-import '@fontsource/inter'
-
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/600.css'
 
 import {UnsplashProvider} from './src/context/unsplash'
 import {MixMasterProvider} from './src/context/mixmaster'
