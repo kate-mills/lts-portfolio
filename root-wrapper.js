36758a34e857c1450b1c565ccef5cfc0ead36554
Kate Mills <helloworldloveksm@icloud.com>
2023-07-09 12:42:19 -0700

    1 'inter' font import

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import '@fontsource/shadows-into-light-two'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/600.css'
+/*import '@fontsource/inter/400.css'
+import '@fontsource/inter/600.css'*/
+import '@fontsource/inter'
+
 
 import {UnsplashProvider} from './src/context/unsplash'
 import {MixMasterProvider} from './src/context/mixmaster'
